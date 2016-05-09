3ee104add0e4a57ea5cb08d72074043cb4022e5a
Matthieu <matthieu.marino@gmail.com>
2016-05-09 10:37:33 +0200

    changed url

--- routes/requestToken.js
+++ routes/requestToken.js
@@ -2,13 +2,13 @@ var request = require('request');
 
 var requestToken = function(code){
 
-    var url = 'https://' + process.env.AUTH0_DOMAIN + '/oauth/token';// +
-        //'client_id=' + process.env.AUTH0_CLIENT_ID +
-        //'&redirect_uri=' + process.env.AUTH0_CALLBACK_URL +
-        //'&client_secret=' + process.env.AUTH0_CLIENT_SECRET +
-        //'&code=' +
-        //code +
-        //'&grant_type=authorization_code';
+    var url = 'https://' + process.env.AUTH0_DOMAIN + '/oauth/token' +
+        'client_id=' + process.env.AUTH0_CLIENT_ID +
+        '&redirect_uri=' + process.env.AUTH0_CALLBACK_URL +
+        '&client_secret=' + process.env.AUTH0_CLIENT_SECRET +
+        '&code=' +
+        code +
+        '&grant_type=authorization_code';
     //console.log(url);
     var options = {
             //method: 'POST',
@@ -16,13 +16,13 @@ var requestToken = function(code){
         port:'3000',
             headers: {
                 'Content-type': 'application/x-www-form-urlencoded'
-            },
-        body:JSON.stringify({client_id: process.env.AUTH0_CLIENT_ID,
-            redirect_uri: process.env.AUTH0_CALLBACK_URL,
-            client_secret: process.env.AUTH0_CLIENT_SECRET,
-            code: code,
-            grant_type: 'authorization_code'
-        })
+            }//,
+        //body:JSON.stringify({client_id: process.env.AUTH0_CLIENT_ID,
+        //    redirect_uri: process.env.AUTH0_CALLBACK_URL,
+        //    client_secret: process.env.AUTH0_CLIENT_SECRET,
+        //    code: code,
+        //    grant_type: 'authorization_code'
+        //})
         };
 
     request.post(options, function (err, res, body) {
